460ea113b02571035dc8f3598a241b4140d23bf7
Michael Burkhardt <p.michael.b@t-online.de>
2017-11-07 20:05:49 +0100

    alle externen Dateien aus description.ext entfernt

--- [J]OPT_v1_04.Tanoa/main/script_macros.hpp
+++ [J]OPT_v1_04.Tanoa/main/script_macros.hpp
@@ -1,6 +1,8 @@
 
 #ifndef EXCLUDE_CBA_MACROS
-    #include "\x\cba\addons\main\script_macros_mission.hpp"
+   // #include "\x\cba\addons\main\script_macros_mission.hpp"
+   // copy the things you need:
+   /* https://github.com/CBATeam/CBA_A3/blob/master/addons/main/script_macros_common.hpp */
 #else
     #define DOUBLES(var1,var2) ##var1##_##var2
     #define TRIPLES(var1,var2,var3) ##var1##_##var2##_##var3
