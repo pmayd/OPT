4c5856208fc888be281239330148deee2374e77c
Jamesadamar <jamesadamar@gmail.com>
2018-11-16 15:06:48 +0100

    refactor cargo component

--- [J]OPT.lythium/cargo/setup.hpp
+++ [J]OPT.lythium/cargo/setup.hpp
@@ -1,4 +1,7 @@
-
+// hier bitte alle Variablen anlegen, die man in irgendeiner Weise
+// einstellen können soll oder irgendwann mal ändern will
+// Beispiele: Add-Action Texte, Variablen, Konstanten, Marker etc
+// #define MEIN_MAKRO ...
 #define OPT_CARGO_ACE_maxWeightCarry 30000
 #define OPT_CARGO_ACE_maxWeightDrag  30000
 
